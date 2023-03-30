9980e664d92ed1cd3d4b18003dda2e39f4d28ea4
Thalita dos Reis <dosreistha@gmail.com>
2023-03-30 14:18:57 +0200

    login form

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,5 +1,24 @@
 import React from "react";
 
 export default function Login() {
-  return <div className="container mt-5 p-5">Login Page</div>;
+  return (
+    <div className="container my-5 p-5">
+      <h2>Login Page</h2>
+      <form>
+        <input
+          className="form-control my-2"
+          placeholder="Username"
+          type="text"
+        />
+        <input
+          className="form-control my-2"
+          placeholder="Password"
+          type="password"
+        />
+        <button className="btn btn-secondary" type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -4,57 +4,52 @@ import axios from "axios";
 
 export default function Register() {
   const navigate = useNavigate();
+
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
     const newUser = { username, email, password };
 
     axios
       .post(`${process.env.REACT_APP_API_URL}/auth/register`, newUser)
-      .then((Response) => {
+      .then((response) => {
         console.log("New user created successfully!");
         navigate("/login");
       })
       .catch((error) => {
-        console.log("Error creating new user");
+        console.log("Error creating new user", error);
       });
   };
 
   return (
-    <div className="container mt-5 p-5">
+    <div className="container my-5 p-5">
+      <h2>Register Page</h2>
       <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Email:
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <br />
-        <button className="btn btn-dark" type="submit">
+        <input
+          className="form-control my-2"
+          placeholder="Username"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          className="form-control my-2"
+          placeholder="Email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className="form-control my-2"
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="btn btn-secondary" type="submit">
           Register
         </button>
       </form>
