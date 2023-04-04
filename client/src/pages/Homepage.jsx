a62de549e562f8df3b0c49227075522790895bee
Thalita dos Reis <dosreistha@gmail.com>
2023-04-04 18:56:52 +0200

    roast grid - homepage

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function Homepage() {
-  return <div className="container m-5 p-5">Homepage</div>;
+  return (
+    <>
+      <div className="container roast-grid">
+        <div className="full-background">Dark Roast</div>
+        <div className="full-background">Medium Roast</div>
+        <div className="full-background">Light Roast</div>
+        <div className="full-background">Decaf</div>
+      </div>
+    </>
+  );
 }
