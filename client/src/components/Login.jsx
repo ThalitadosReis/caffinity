53185b6305a8bf9083aff805742ef78d38acaa52
Thalita dos Reis <dosreistha@gmail.com>
2023-04-04 17:29:28 +0200

    design of register-login page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,29 +3,37 @@ import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
-    <div className="container my-5 p-5">
-      <h2>Login Page</h2>
-      <form>
-        <input
-          className="form-control my-2"
-          placeholder="Username"
-          type="text"
+    <div className="simple-grid overflow-hidden">
+      <div className="simple-grid__cell simple-grid__cell--fill p-0">
+        <img
+          src="https://images.pexels.com/photos/851555/pexels-photo-851555.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="Coffee"
         />
-        <input
-          className="form-control my-2"
-          placeholder="Password"
-          type="password"
-        />
-        <button className="btn btn-secondary" type="submit">
-          Login
-        </button>
+      </div>
+      <div className="simple-grid__cell simple-grid__cell--1/3 my-auto">
+        <h2>Login</h2>
+        <form>
+          <input
+            className="form-control my-2"
+            placeholder="Username"
+            type="text"
+          />
+          <input
+            className="form-control my-2"
+            placeholder="Password"
+            type="password"
+          />
+          <button className="btn btn-secondary mb-2" type="submit">
+            Login
+          </button>
 
-        <br />
-        <Link to="/register">
-          New to Caffinity?
-          <span> Register</span>
-        </Link>
-      </form>
+          <br />
+          <Link to="/register">
+            New to Caffinity?
+            <span> Register</span>
+          </Link>
+        </form>
+      </div>
     </div>
   );
 }
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -25,41 +25,48 @@ export default function Register() {
   };
 
   return (
-    <div className="container my-5 p-5">
-      <h2>Register Page</h2>
+    <div className="simple-grid overflow-hidden">
+      <div className="simple-grid__cell simple-grid__cell--fill p-0">
+        <img
+          src="https://images.pexels.com/photos/1724194/pexels-photo-1724194.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="Coffee"
+        />
+      </div>
+      <div className="simple-grid__cell simple-grid__cell--1/3 my-auto">
+        <h2>Register</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="form-control my-2"
+            placeholder="Username"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            className="form-control my-2"
+            placeholder="Email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            className="form-control my-2"
+            placeholder="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="btn btn-secondary mb-2" type="submit">
+            Register
+          </button>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          className="form-control my-2"
-          placeholder="Username"
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          className="form-control my-2"
-          placeholder="Email"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          className="form-control my-2"
-          placeholder="Password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="btn btn-secondary" type="submit">
-          Register
-        </button>
-
-        <br />
-        <Link to="/login">
-          Already have an account?
-          <span> Login</span>
-        </Link>
-      </form>
+          <br />
+          <Link to="/login">
+            Already have an account?
+            <span> Login</span>
+          </Link>
+        </form>
+      </div>
     </div>
   );
 }
